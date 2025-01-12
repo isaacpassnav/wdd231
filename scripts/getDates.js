c646e3971a586ac15b1dfeca7964b63d24281102
Your Name <pasapera279@gmail.com>
2025-01-12 00:20:59 -0500

    menu hamburguer

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -3,3 +3,19 @@ document.getElementById('currentyear').textContent = currentYear;
 
 const lastModifiedDate = document.lastModified;
 document.getElementById('lastModified').textContent = `Last Modified: ${lastModifiedDate}`;
+
+// Selecciona los elementos del DOM
+const mainNav = document.querySelector(".nav");
+const hamButton = document.querySelector("#menu");
+
+// Verifica si los elementos existen antes de agregar el evento
+if (mainNav && hamButton) {
+    hamButton.addEventListener("click", () => {
+        // Alternar la clase 'show' para mostrar u ocultar el menú
+        mainNav.classList.toggle("show");
+        hamButton.classList.toggle("show"); // Cambia el ícono entre ≡ y X
+    });
+}
+
+
+
